88aab9024619234395a23a6eba670a1bf3aefedc
whittydev <whittymusic@gmail.com>
2021-02-23 21:09:49 -0600

    complete project

---
+++ js/script.js
@@ -0,0 +1,17 @@
+//get element
+const body = document.querySelector("body");
+const button = document.querySelector("button");
+
+//color array
+const colors = ["red", "blue", "green", "yellow", "orange"];
+
+body.style.backgroundColor = "red";
+
+//event listener
+button.addEventListener("click", changeColors);
+
+//change color with math.random
+function changeColors() {
+  const colorIndex = parseInt(Math.random() * colors.length);
+  body.style.backgroundColor = colors[colorIndex];
+}
